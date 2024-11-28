11d0457d862643bc8cb6e4b4bfbb85eb0fd28d5f
Leonardo Souzas <leonardomellolopes@gmail.com>
2024-11-28 20:55:35 -0300

    primeiro comentario dessa atividade

---
+++ src/exercicio1.ts
@@ -0,0 +1,28 @@
+//Instruções
+//1. Utilizando (While), construir um programa que apresente a soma dos
+//cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+
+let numero = 1;
+let soma = 0;
+while (numero <= 100) {
+soma = soma + numero++;
+console.log("a soma é: "  + soma);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/exercicio2.ts
@@ -0,0 +1,3 @@
+//2. Utilizando (DoWhile), elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 500.
+
+let valor = 0;
