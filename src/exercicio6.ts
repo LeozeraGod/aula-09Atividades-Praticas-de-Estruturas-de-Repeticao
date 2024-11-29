5ffac9eca6e412412dcadb867da551d0ed29968f
Leonardo Souzas <leonardomellolopes@gmail.com>
2024-11-28 21:58:22 -0300

    ultima alteração

--- src/exercicio6.ts
+++ src/exercicio6.ts
@@ -3,13 +3,13 @@
 //O programa deve apresentar os valores das duas temperaturas. Para converter grau Celsius para Fahrenheit,
 //basta multiplicar a temperatura em graus Celsius por 1,8 e somar 32.
 
-let celsius = 0;
-let fahrenheit =0;
+let cs = 0;
+let ft = 0;
 
-for(let i = 1; i<=100; i++){
-  if(!(i%10)){
-  celsius = i;
-  fahrenheit = (celsius*1.8) +32;
-  console.log("Graus Célcius é = " + celsius + " graus e Graus Fahrenheit = "+ fahrenheit);
-}
+for (let i = 1; i <= 100; i++) {
+    if (!(i % 10)) {
+        cs = i;
+        ft = (cs * 1.8) + 32;
+        console.log("Graus Célcius é = " + cs + " graus e Graus Fahrenheit = " + ft);
+    }
 }
