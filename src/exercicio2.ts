025bb875388623bbacf0c916498e15bd237d8407
Leonardo Souzas <leonardomellolopes@gmail.com>
2024-11-28 21:54:18 -0300

    Conclusão das atividades

--- src/exercicio2.ts
+++ src/exercicio2.ts
@@ -1,3 +1,11 @@
 //2. Utilizando (DoWhile), elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 500.
 
-let valor = 0;
+let soma2: number = 0;
+let par = 2;
+
+do {
+    soma2 += par;
+    par += 2;
+} while (par <= 500)
+
+    console.log("A soma total dos numeros de 1 a 500 é:  " + soma2);
